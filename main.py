e9fcfbc4f5d905bf13c8417a1c736093aa70b718
demiancorrea <demiannataniel.correa32@gmail.com>
2025-08-21 10:12:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+intentos = 7
+palabras = ["computadora", "tecnología"]
+palabra_=[]
+palabra_elegida = random.choice(palabras)
+for x in range(len(palabra_elegida)):
+    palabra_.append("_")
+while intentos>0:
+    print(f"{palabra_} intentos restantes: {intentos}")
+    letra_elegida=input("ingrese la letra elegida:")
+    adivino=False
+    for indice_letra in range (len(palabra_elegida)):
+        for letra2 in letra_elegida:
+            if letra2==palabra_elegida[indice_letra]:
+                palabra_[indice_letra]=letra2
+                adivino=True
+    if not adivino:
+        intentos-=1
+    if not "_" in palabra_:
+        print("ganaste sos un chad.")
+        break
+if intentos<=0:
+    print("perdiste bobo.")
+
+
