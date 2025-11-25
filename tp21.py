4879d90296d3a00d41e1f9d9416ce39c170b117d
demiancorrea <demiannataniel.correa32@gmail.com>
2025-11-25 12:53:41 -0300

    Add files via upload

---
+++ tp21.py
@@ -0,0 +1,60 @@
+def factorial(n):
+   if n == 0:
+       return 1
+   else:
+       return n * factorial(n-1)
+   print(factorial(5))
+
+
+def suma_lista(lista):
+   resultado = lista[0]+lista[1]+lista[2]+lista[3]
+   return resultado
+   print(suma_lista([1,2,3,4]))
+
+
+class medicamento():
+   def __init__(self,nombre, precio, categoria, stock, codigo_de_barra):
+       self.nombre = nombre
+       self.precio = precio
+       self.categoria = categoria
+       self.stock = stock
+       self.codigo_de_barra = codigo_de_barra
+
+   def reponer_stock(self, cantidad):
+       self.stock = self.stock + cantidad
+
+
+   def vender(self):
+       cantidad_producto = int(input("Ingrese cantidad a comprar: "))
+       aux = self.stock
+       self.stock = self.stock - cantidad_producto
+       if self.stock < 0:
+           print("No hay suficiente Cantidad")
+           self.stock = aux
+       return self.stock
+
+
+   def esta_en_stock_critico(self):
+       if self.stock<10:
+           return True
+       else:
+           return False
+
+
+ibuprofeno = medicamento("Ibuprofeno 600mg", 150.0, "aliviante", 80, 1120893645)
+paracetamol = medicamento("Paracetamol 500mg", 120.0, "aliviante",100, 1207336271)
+print(ibuprofeno.nombre,ibuprofeno.precio,ibuprofeno.categoria,ibuprofeno.stock,ibuprofeno.codigo_de_barra,ibuprofeno.esta_en_stock_critico())
+print(paracetamol.nombre,paracetamol.precio,paracetamol.categoria,paracetamol.stock,paracetamol.codigo_de_barra,paracetamol.esta_en_stock_critico())
+seguimos = True
+while seguimos:
+    eleccion = int(input("""Elija que producto quiere comprar:
+          1. Ibuprofeno 600mg 
+          2. Paracetamol 500mg
+          3. Salir: """))
+    if eleccion == 1:
+        print(f"El stock Actual del Ibuprofeno es:",ibuprofeno.vender())
+    elif eleccion == 2:
+        print("El stock Actual del Paracetamol es:",paracetamol.vender())
+    elif eleccion == 3:
+        print("Saliendo del Programa...")
+        seguimos = False
