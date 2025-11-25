9a24af9514081bc6e1174f0eb8afc6be7704a372
demiancorrea <demiannataniel.correa32@gmail.com>
2025-11-25 13:08:31 -0300

    Update tp21.py

--- tp21.py
+++ tp21.py
@@ -20,10 +20,11 @@ class medicamento():
        self.stock = stock
        self.codigo_de_barra = codigo_de_barra
 
-   def reponer_stock(self, cantidad):
-       self.stock = self.stock + cantidad
-
-
+   def reponer(self):
+       cantidad_reponer = int(input("Ingrese La cantidad a reponer: "))
+       aux = self.stock
+       self.stock = self.stock + cantidad_reponer
+       return self.stock
    def vender(self):
        cantidad_producto = int(input("Ingrese cantidad a comprar: "))
        aux = self.stock
@@ -45,16 +46,31 @@ ibuprofeno = medicamento("Ibuprofeno 600mg", 150.0, "aliviante", 80, 1120893645)
 paracetamol = medicamento("Paracetamol 500mg", 120.0, "aliviante",100, 1207336271)
 print(ibuprofeno.nombre,ibuprofeno.precio,ibuprofeno.categoria,ibuprofeno.stock,ibuprofeno.codigo_de_barra,ibuprofeno.esta_en_stock_critico())
 print(paracetamol.nombre,paracetamol.precio,paracetamol.categoria,paracetamol.stock,paracetamol.codigo_de_barra,paracetamol.esta_en_stock_critico())
-seguimos = True
-while seguimos:
-    eleccion = int(input("""Elija que producto quiere comprar:
-          1. Ibuprofeno 600mg 
-          2. Paracetamol 500mg
-          3. Salir: """))
-    if eleccion == 1:
-        print(f"El stock Actual del Ibuprofeno es:",ibuprofeno.vender())
-    elif eleccion == 2:
-        print("El stock Actual del Paracetamol es:",paracetamol.vender())
-    elif eleccion == 3:
-        print("Saliendo del Programa...")
-        seguimos = False
+def comprar():
+    seguimos = True
+    while seguimos:
+        eleccion = int(input("""Elija que producto quiere comprar:
+              1. Ibuprofeno 600mg 
+              2. Paracetamol 500mg
+              3. Salir: """))
+        if eleccion == 1:
+            print(f"El stock Actual del Ibuprofeno es:",ibuprofeno.vender())
+        elif eleccion == 2:
+            print("El stock Actual del Paracetamol es:",paracetamol.vender())
+        elif eleccion == 3:
+            print("Saliendo del Programa...")
+            seguimos = False
+comprar()
+
+def reponer():
+    seguimos = True
+    while seguimos:
+        escoger_producto = (
+            int(input("""Ingrese El producto al cual reponer su stock:
+                                1. Ibuprofeno 600mg
+                                2. Paracetamol 500mg: """)))
+        if escoger_producto == 1:
+            print("Cantidad Actual: ",ibuprofeno.reponer())
+        elif escoger_producto == 2:
+            print("Cantidad Actual: ",paracetamol.reponer())
+reponer()
